cd57689ea626251c2db97349db07f8e20341c2d3
docker-py3 <docker-py3@gmail.com>
2021-10-08 11:03:02 +0000

    添加用户鉴权

--- voice_api/blueprints/fs_api/views.py
+++ voice_api/blueprints/fs_api/views.py
@@ -1,39 +1,42 @@
-from flask import Blueprint,render_template,request,make_response
+from flask import Blueprint,render_template,request,make_response,current_app
 from voice_api.blueprints.ext import Ext
 
 fs_api=Blueprint('fs_api',__name__,template_folder='templates')
 
 @fs_api.route('/api/auth-ext',methods=['POST'])
 def auth_ext():
-    r=request.form
-    #print(r)
-    sip_auth_username=r.get('user')
-    domain=r.get('domain')
-    if r.get('Event-Calling-Function')== 'switch_load_network_lists':
-        response=make_response(render_template('404.xml'))
-        response.headers['Content-Type'] = 'application/xml'
-        return response
-    elif r.get('Event-Calling-Function')=='config_sofia':
-        response=make_response(render_template('404.xml'))
-        response.headers['Content-Type'] = 'application/xml'
-        return response
-    else:
-        #查询数据库返回xml
-        try:
-            ext=Ext.query({'domain':domain,'extnumber':sip_auth_username})
-        except AttributeError:
+    r_token=request.args['tk']
+    if r_token in current_app.config['FS_TOKEN']:
+        r=request.form
+        #print(r)
+        sip_auth_username=r.get('user')
+        domain=r.get('domain')
+        if r.get('Event-Calling-Function')== 'switch_load_network_lists':
             response=make_response(render_template('404.xml'))
             response.headers['Content-Type'] = 'application/xml'
             return response
-        if ext['extnumber'] in ['1000','1001']:
+        elif r.get('Event-Calling-Function')=='config_sofia':
             response=make_response(render_template('404.xml'))
             response.headers['Content-Type'] = 'application/xml'
             return response
         else:
-            response=make_response(render_template('ext.xml',ext=ext))
-            response.headers['Content-Type'] = 'application/xml'
-            return response
-
+            #查询数据库返回xml
+            try:
+                ext=Ext.query({'domain':domain,'extnumber':sip_auth_username})
+            except AttributeError:
+                response=make_response(render_template('404.xml'))
+                response.headers['Content-Type'] = 'application/xml'
+                return response
+            if ext['extnumber'] in ['1000','1001']:
+                response=make_response(render_template('404.xml'))
+                response.headers['Content-Type'] = 'application/xml'
+                return response
+            else:
+                response=make_response(render_template('ext.xml',ext=ext))
+                response.headers['Content-Type'] = 'application/xml'
+                return response
+    else:
+        return 'Auth Fail',401
 @fs_api.route('/api/queue-info',methods=['POST'])
 def queue_info():
     r=request
